d1c6f8f4bdbabbe114634f31b131d9dda7f1f874
karbal abdelhadi <abdoraja2005@gmail.com>
2024-05-28 22:34:10 +0100

    add 4-index.html

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -32,7 +32,7 @@ def get_locale() -> Optional[str]:
 @app.route('/')
 def index() -> str:
     """first root"""
-    return render_template("3-index.html")
+    return render_template("4-index.html")
 
 
 if __name__ == "__main__":
